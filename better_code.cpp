e84be78f22a05b6c0323417f294e8690b0a1321e
Mark Rebane <mark.rebane@gmail.com>
2018-11-23 20:11:26 +1030

    Runtime Polymorphism: Basic document_t: no polymorphism
    Sean Parent's guidelines
    Write all code as a library
     * Reuse increases your productivity
     * Writing unit tests is simplified

---
+++ better_code.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+using object_t = int;
+
+void draw(const object_t& x, ostream& out, size_t position)
+{
+    out << string(position, ' ') << x << endl;
+}
+
+using document_t = vector<object_t>;
+
+void draw(const document_t& x, ostream& out, size_t position)
+{
+    out << string(position, ' ') << "<document>" << endl;
+    for (const auto& e : x) {
+        draw(e, out, position + 2);
+    }
+    out << string(position, ' ') << "</document>" << endl;
+}
+
+int main()
+{
+    document_t document;
+
+    document.emplace_back(0);
+    document.emplace_back(1);
+    document.emplace_back(2);
+    document.emplace_back(3);
+
+    draw(document, cout, 0);
+}
