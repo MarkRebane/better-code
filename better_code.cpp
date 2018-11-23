6d0bc0a121474a63ef5c4ead45463ee6c3cb4ce2
Mark Rebane <mark.rebane@gmail.com>
2018-11-23 20:39:18 +1030

    Runtime Polymorphism: Basic document_t: naive polymorphism
    Deep problem no.1
    Changed the semantics of copy, assignment, and equality of document
     * leads to incidental data-structures
     * thread safety concerns
    We define an operation in terms of the operation's semantics:
     "Assignment is a procedure taking two objects of the same type that
     makes the first object equal to the second without modifying the
     second." - Elements of Programming, Section 1.5
    A shared pointer is as good as a global variable.
     "There is a set of procedures whose inclusion in the computational
     basis of a type lets us place objects in data structures and use
     algorithms to copy objects from one data structure to another. We call
     these types having such a basis _regular_, since their use guarantees
     regularity of behavior and, therefore, interoperability." - Elements of
     Programming, Section 1.5
    
    Deep problem no.2
    Inefficient
     * calls to draw() on my_class_t are often indirect through virtual
     calls, including the destructor
     * my_class_t is _always_ heap allocated
     * acces to my class_t must be synchronized
    
    Deep problem no.3
    Polymorphism is intrusive
     * Document can no longer hold a drawable integer
    The requirement of a polymorphic type, by definition, comes from its use
    There are no polymorphic types, only a _polymorphic use_ of similar
    types
    By using inheritance to capture polymorphic use, we shift the burden of
    use to the type implementation, tightly coupling components
    Inheritance implies variable size, which implies heap allocation
    Heap allocation forces a further burden to manage the object lifetime
    Indirect, heap allocation, and virtualisation, impacts performance
    Object lifetime management leads to garbage collection or reference
    counting
    This encourages _shared_ ownership and the proliferation of _incidental
    data-structures_
    Shared ownership leads to synchronisation issues, breaks local
    reasoning, and further impacts performance
    
    Inheritance is the base class of Evil.

--- better_code.cpp
+++ better_code.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
+#include <memory>
 #include <vector>
 
 using namespace std;
 
-using object_t = int;
+class object_t {
+  public:
+    virtual ~object_t() {}
+    virtual void draw(ostream&, size_t) const = 0;
+};
 
-void draw(const object_t& x, ostream& out, size_t position)
-{
-    out << string(position, ' ') << x << endl;
-}
-
-using document_t = vector<object_t>;
+using document_t = vector<shared_ptr<object_t>>;
 
 void draw(const document_t& x, ostream& out, size_t position)
 {
     out << string(position, ' ') << "<document>" << endl;
     for (const auto& e : x) {
-        draw(e, out, position + 2);
+        e->draw(out, position + 2);
     }
     out << string(position, ' ') << "</document>" << endl;
 }
+
+class my_class_t final : public object_t {
+  public:
+    void draw(ostream& out, size_t position) const override
+    {
+        out << string(position, ' ') << "my_class_t" << endl;
+    }
+};
 
 int main()
 {
     document_t document;
 
-    document.emplace_back(0);
-    document.emplace_back(1);
-    document.emplace_back(2);
-    document.emplace_back(3);
+    document.emplace_back(make_shared<my_class_t>());
 
     draw(document, cout, 0);
 }
